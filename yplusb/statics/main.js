6e6e0abf1012867d446cc2cf8176eec5f9a25b9f
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2022-02-15 18:01:36 +0530

    yplusb project

---
+++ yplusb/statics/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	setTimeout(function(){
+		$("img").addClass("animated");
+	}, 4000);
+	setTimeout(function(){
+		$(".animation-1").addClass("animated");
+	}, 4000);
+	setTimeout(function(){
+		$(".animation-2").addClass("animated");
+	}, 6000);
+})
---
+++ yplusb/yplusbapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YplusbappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'yplusbapp'
